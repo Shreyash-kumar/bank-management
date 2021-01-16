c59e24e2a87a638a303256eb3e8e9f0fdaf52cbb
Shreyash Kumar Sahoo <76588629+Shreyash-kumar@users.noreply.github.com>
2021-01-16 16:21:06 +0530

    Update banking_System.cpp

--- banking_System.cpp
+++ banking_System.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-#include <conio.h>
 #include <string>
+
 using namespace std;
 
 class Bank{
@@ -17,38 +17,21 @@ class Bank{
     string branch;
     long long  int phone_no;
     
-    void EnterDetails();//For inputing details
+   
     
     void show_details();//For outputting details of the account 
-};
 
-void Bank::EnterDetails(){
-        
-
-        cout<<"Enter branch name: ";
-        getline(cin, branch);
-      
-       
-        cout<<endl;
-
-        cout<<"Enter your account number: ";
-        cin>>account_no;
-        cout<<endl;
-
-        cout<<"Enter name:";
-        getline(cin, name);
-        cout<<endl;
+    Bank(long long int account_no1 , string name1  , string branch1 , long long int phone_no1 , int pin1){
+         
+         name=name1;
+         account_no=account_no1;
+         branch=branch1;
+         phone_no=phone_no1;
+         pin=pin1;
+    
+    }// Parameterized constructor to initialize values  
   
-        cout<<"Enter phone number: ";
-        cin>>phone_no;
-        cout<<endl;
-
-        cout<<"Set your 4 digit pin: ";
-        cin>>pin;
-        cout<<endl;
-
-      
-}
+};
 
 
 void Bank::show_details(){
@@ -64,29 +47,54 @@ void Bank::show_details(){
 
 int main(){
 
-    Bank a[100];
+    long long int account_no , phone_no ; 
+    string name , branch ;
+    int pin ; 
 
     int i ,n ;
-    
+
     cout<<"Enter the number of account you want to create: ";
     cin>>n;
 
+    Bank* a = (Bank*)malloc(sizeof(Bank)*n) ;
+
     for(i = 0 ; i < n ; i ++){
 
-        cout<<"enter details of "<<i+1<<" account : "<<endl;
-        a[i].EnterDetails();
+        cout<<"enter details of "<<i+1<<" account : "<<endl<<endl;
+        
+        cout<<"Enter branch name: ";
+        cin>>branch; 
+        cout<<endl;
+
+        cout<<"Enter your account number: ";
+        cin>>account_no;
+        cout<<endl;
+
+        cout<<"Enter name(use  instead underscore '_' instead  of space ):";
+        cin>>name;
+        cout<<endl;
+  
+        cout<<"Enter phone number: ";
+        cin>>phone_no;
+        cout<<endl;
+
+        cout<<"Set your 4 digit pin: ";
+        cin>>pin;
+        cout<<endl;
+        
+        a[i] = Bank(account_no , name, branch, phone_no, pin );
 
     }
 
 
     for(i = 0 ; i < n ; i ++){
 
-        cout<<"Details of "<<i+1<<" account are : "<<endl;
+        cout<<"Details of "<<i+1<<" account are : "<<endl<<endl;;
         a[i].show_details();
 
     }
 
 
-    getch();
+   
     return 0;
 }
