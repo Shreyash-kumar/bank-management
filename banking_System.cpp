a09e199696c59ea5bee6dd7c128b7b03c52fea66
Shreyash Kumar Sahoo <76588629+Shreyash-kumar@users.noreply.github.com>
2021-01-19 17:03:13 +0530

    Update banking_System.cpp

--- banking_System.cpp
+++ banking_System.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
-#include <conio.h>
-#include <string>
+
 using namespace std;
 
 class Bank{
@@ -17,23 +16,62 @@ class Bank{
     string branch;
     long long  int phone_no;
     
-    void EnterDetails();//For inputing details
+   
     
     void show_details();//For outputting details of the account 
+
+    Bank(long long int account_no1 , string name1  , string branch1 , long long int phone_no1 , int pin1){
+         
+         name=name1;
+         account_no=account_no1;
+         branch=branch1;
+         phone_no=phone_no1;
+         pin=pin1;
+    
+    }// Parameterized constructor to initialize values  
+  
 };
 
-void Bank::EnterDetails(){
+
+void Bank::show_details(){
+
+      cout<<"Account number: "<<account_no<<endl<<endl;
+      cout<<"Name: "<<name<<endl<<endl;
+      cout<<"Phone number: "<<phone_no<<endl<<endl;
+      cout<<"Branch: "<<branch<<endl<<endl;
+      cout<<"pin: "<<pin<<endl;             
+
+}
+
+
+int main(){
+
+    long long int account_no , phone_no ; 
+    string name , branch ;
+    int pin ; 
+
+    int i ,n ;
+
+    cout<<"Enter the number of account you want to create: ";
+    cin>>n;
+
+    Bank* a = (Bank*)malloc(sizeof(Bank)*n) ;
+
+    for(i = 0 ; i < n ; i ++){
+
+        cout<<"enter details of "<<i+1<<" account : "<<endl<<endl;
         
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
         cout<<"Enter branch name: ";
-        getline(cin, branch);
-      
-       
+        getline(cin, branch); 
         cout<<endl;
 
         cout<<"Enter your account number: ";
         cin>>account_no;
         cout<<endl;
+        
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
         cout<<"Enter name:";
         getline(cin, name);
@@ -46,47 +84,20 @@ void Bank::EnterDetails(){
         cout<<"Set your 4 digit pin: ";
         cin>>pin;
         cout<<endl;
-
-      
-}
-
-
-void Bank::show_details(){
-
-      cout<<"Account number: "<<account_no<<endl<<endl;
-      cout<<"Name: "<<name<<endl<<endl;
-      cout<<"Phone number: "<<phone_no<<endl<<endl;
-      cout<<"Branch: "<<branch<<endl<<endl;
-      cout<<"pin: "<<pin;             
-
-}
-
-
-int main(){
-
-    Bank a[100];
-
-    int i ,n ;
-    
-    cout<<"Enter the number of account you want to create: ";
-    cin>>n;
-
-    for(i = 0 ; i < n ; i ++){
-
-        cout<<"enter details of "<<i+1<<" account : "<<endl;
-        a[i].EnterDetails();
+        
+        a[i] = Bank(account_no , name, branch, phone_no, pin );
 
     }
 
 
     for(i = 0 ; i < n ; i ++){
 
-        cout<<"Details of "<<i+1<<" account are : "<<endl;
+        cout<<"Details of "<<i+1<<" account are : "<<endl<<endl;
         a[i].show_details();
 
     }
 
 
-    getch();
+   
     return 0;
 }
